d0abd355c743f68b33f9dbec60f3e97550fc5d4b
Daniel Baptista <danielbaptista98@gmail.com>
2023-11-23 23:50:24 +0000

    Initial commit

---
+++ src/main/java/com/morceguito/training/webappsecurity/WebAppSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.morceguito.training.webappsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morceguito/training/webappsecurity/WebAppSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morceguito.training.webappsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
